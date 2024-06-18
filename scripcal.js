1c0f613d93dd37cac5878d4b38f38ad7b84e3177
james-zuluaga <jameszuluaga6@gmail.com>
2024-06-18 18:07:40 -0500

    III

--- scripcal.js
+++ scripcal.js
@@ -1,4 +1,4 @@
- /// ejercio en los cuales sacamos la info de html///////
+ /// calcular valor energia ///////
 
  let consumo301=0;
  let consumo302=0;
@@ -47,3 +47,47 @@
       let sumTotalSer=consumo301+consumo302+consumo401;
       document.getElementById('sumTotalSer').textContent = 'el total a pagar del recibo es : $' + sumTotalSer;
      }
+
+
+     //////////////////////// calcular acueducto
+
+     // aseo 
+
+let aResidencial=0;
+ let subsidioRes=0;
+ let cargofiNores=0;
+ let aporte=0;
+ let toAseo=0;  
+ let totalRec=0;
+ let aBodega=0;
+
+ //// acueducto
+let vaAcueducto=0;
+let personas=0;
+let valAcuXPer=0; // secalcula
+let totalbodega=0; // se calcula
+let acue201=0; // se opera
+let toPago201=0;// se opera
+
+
+ function sumaAc201(){
+
+   let suma301=0;
+   totalRec=Number(document.getElementById(`pal10`).value);
+//   aResidencial=Number(document.getElementById(`pal11`).value);
+   subsidioRes=Number(document.getElementById(`pal12`).value);
+   cargofiNores=Number(document.getElementById(`pal13`).value);
+   aporte=Number(document.getElementById(`pal14`).value);
+     toAseo=Number(document.getElementById(`pal15`).value);
+     personas=Number(document.getElementById(`pal16`).value);
+     aBodega=aporte+cargofiNores;
+     aResidencial=toAseo-aBodega;
+     valAcuXPer=vaAcueducto/personas;
+
+   vrsub301=vrKwSin301/2;
+   resultado301= vfinal301-vinicial301;
+   consumo301= resultado301*vrKwSin301;
+      document.getElementById('resultado301').textContent = 'los Kw consumidos son : ' + resultado301;
+      document.getElementById('consumo301').textContent = 'el valor a pagar es: ' + consumo301;
+      document.getElementById('vrsub301').textContent = 'el valor del kw con subsidio es: $' + vrsub301;
+ }
